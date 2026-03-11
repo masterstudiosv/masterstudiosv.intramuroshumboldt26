3e5af1a7379206db0e2811eccbf6d1f1ba708dde
masterstudiosv <masterstudiosv@gmail.com>
2026-03-10 18:19:43 -0600

    Update APPS_SCRIPT_URL in config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyUyEiufNV-jRMJhskKUAoVcJWL3gHfScrcPKydPMHBbpC72ua0kJSwGptOMrC2HUBHjw/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwE3zLzJ0-1T9SFautkTj4nrsDZiSIlKxbDU6Dkuwg2gKqz4WQlzdeGBL0YpmA2z77A-Q/exec',
   WRITE_TOKEN: 'Humb0ldt#2026$xK9',
   APP_NAME:      'Intramuros Humboldt 2026',
   SCHOOL_NAME:   'Complejo Educativo Alejandro de Humboldt',
